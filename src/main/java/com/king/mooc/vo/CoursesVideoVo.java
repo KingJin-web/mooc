34d557dd480f03318c2eb060e42407d993ed67a7
KingJin-web <1261337209@qq.com>
2021-10-24 20:48:58 +0800

    ✔ 课程章节信息

--- src/main/java/com/king/mooc/vo/CoursesVideoVo.java
+++ src/main/java/com/king/mooc/vo/CoursesVideoVo.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -16,10 +17,10 @@ import java.time.LocalDateTime;
  */
 @Data
 public class CoursesVideoVo {
-
-
+    @ApiModelProperty(value = "视频小节名")
     private String name;
-    private LocalDateTime createTime;
+    @ApiModelProperty(value = "章节名")
     private String url;
+    @ApiModelProperty(value = "章节名")
     private int learn_times;
 }
