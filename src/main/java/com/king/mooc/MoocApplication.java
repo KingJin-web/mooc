556c0dfa932ba34121c0903b5e6d8563fa2ea951
KingJin-web <1261337209@qq.com>
2022-04-25 22:42:00 +0800

    引入百度统计

--- src/main/java/com/king/mooc/MoocApplication.java
+++ src/main/java/com/king/mooc/MoocApplication.java
@@ -11,15 +11,6 @@ import org.springframework.http.converter.HttpMessageConverter;
 
 @SpringBootApplication
 public class MoocApplication {
-//    @Bean
-//    public HttpMessageConverters fastJsonHttpMessageConverters() {
-//        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
-//        FastJsonConfig fastJsonConfig = new FastJsonConfig();
-//        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
-//        fastConverter.setFastJsonConfig(fastJsonConfig);
-//        return new HttpMessageConverters(fastConverter);
-//    }
-
     public static void main(String[] args) {
         SpringApplication.run(MoocApplication.class, args);
     }
